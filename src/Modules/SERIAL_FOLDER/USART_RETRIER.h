b96368596dadab569a5f2bf085d526df018671de
cristianeanjos773-cyber <cristianeanjos773@gmail.com>
2026-08-11 22:21:52 -0300

    Commit before USART_RETRIER reaches devphase1

---
+++ src/Modules/SERIAL_FOLDER/USART_RETRIER.h
@@ -0,0 +1,7 @@
+#pragma once 
+
+/*
+Purpose: this will be a moduule that will take care of retrying to send again the failed USART message incase of fails 
+*/
+
+void USART_RETRY(char FAILED_MESSAGE); 
